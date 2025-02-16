0a04a7549a7a10645126de0105d06a90a3aef5bb
Rabab <raaa555@outlook.com>
2025-02-16 20:08:12 +0300

    finshed classes in kotlin exercise

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,21 @@
+import java.awt.print.Book
+
 fun main() {
-    println("Hello World!")
+
+    var myBook = Book(title ="moonstone",  author= "Richerd", genre="fiction",  yearPublished =2020)
+    myBook.getBookInfo()
+//    myBook.title = "moonstone"
+//    myBook.author = "Richerd"
+//    myBook.yearPublished = 2020
+//    println(myBook.title + " " + myBook.author + " " + myBook.yearPublished)
+}
+
+class Book(var title: String, var author: String, var genre: String, var yearPublished: Int) {
+//    var title: String = ""
+//    var author: String = ""
+//    var yearPublished: Int = 0
+
+    fun getBookInfo() {
+        println(" title: $title, author: $author, genre: $genre, yearPublished: $yearPublished")
+    }
 }
